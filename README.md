c64411da9ff07d343acb9c75652206b9842985be
Nitish Singh <singhnitish198@gmail.com>
2023-11-13 00:47:17 +0530

    Just added few Pages and configurations

---
+++ README.md
@@ -0,0 +1,10 @@
+## 3D Portfolio Website for me
+
+> See it live [Over Here!]()
+
+> Check out my other [site here!](https://nitish1.netlify.app)
+
+## Dependencies
+
+- React Router Dom
+- @react-three/fiber
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+	return (
+		<section className="w-full h-screen relative">
+			{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+				POP UP!
+			</div> */}
+		</section>
+	);
+};
+
+export default Home;
